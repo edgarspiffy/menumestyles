efb5ac24a36ec45c6dd4a546adfdc73b3aed3361
Edgar Limon <edgarlimon805@gmail.com>
2020-10-17 14:43:50 -0700

    fixing up

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dish = require('../models/dish');
+const spot = require('../models/restaurant');
+const router = express.Router();
+const restaurant = require('../models/restaurant');
+
+
+
+
+
+
+
+
+
+module.exports = router;
